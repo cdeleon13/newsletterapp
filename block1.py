346e42704b91bde7cac8a8da5aeb74cee297fedb
cdeleon13 <115028409+cdeleon13@users.noreply.github.com>
2024-04-15 14:51:08 -0700

    Update block1.py

--- block1.py
+++ block1.py
@@ -6,7 +6,7 @@ block1 = html.Div(
     children=[
         dbc.Card(
             [
-                dbc.CardHeader("What's new?", className='display-4'),
+                dbc.CardHeader("What's new?", className='display-4', style={"font-weight":"bolder","background-color":'#0075C2', "color":"white"}),
                 dbc.CardBody(
                     [
                         dbc.Row(
@@ -17,8 +17,8 @@ block1 = html.Div(
                                         className="card card-container",
                                         children=[
                                             html.Div(className="plots", children=[dcc.Graph(id='fth_race_plot', className='race_plot')]),
-                                            html.H3(children="First Time Homeless", className='display-5', style={"text-align": "center", "position": "absolute", "top": "45%", "left": "50%", "transform": "translate(-50%, -50%)"}),
-                                            html.H3(id="fth_count", className="metric", style={"text-align": "center", "position": "absolute", "top": "55%", "left": "50%", "transform": "translate(-50%, -50%)"}),
+                                            html.H3(children="First Time Homeless", className='display-5', style={"background-color":'#0075C2',"color":"white", "text-align": "center", "position": "absolute", "top": "45%", "left": "50%", "transform": "translate(-50%, -50%)"}),
+                                            html.H3(id="fth_count", className="metric", style={"font-weight":"bolder","color":"#0075C2","text-align": "center", "position": "absolute", "top": "55%", "left": "50%", "transform": "translate(-50%, -50%)"}),
                                         ],
                                         style={"position": "relative", "height": "100%"}
                                     ),
@@ -29,8 +29,8 @@ block1 = html.Div(
                                         className="card card-container",
                                         children=[
                                             html.Div(className="plots", children=[dcc.Graph(id='housed_race_plot', className='race_plot')]),
-                                            html.H3(children="Persons Housed", className='display-5', style={"text-align": "center", "position": "absolute", "top": "45%", "left": "50%", "transform": "translate(-50%, -50%)"}),
-                                            html.H3(id="housed_count", className="metric", style={"text-align": "center", "position": "absolute", "top": "55%", "left": "50%", "transform": "translate(-50%, -50%)"}),
+                                            html.H3(children="Persons Housed", className='display-5', style={"background-color":'#0075C2',"color":"white","text-align": "center", "position": "absolute", "top": "45%", "left": "50%", "transform": "translate(-50%, -50%)"}),
+                                            html.H3(id="housed_count", className="metric", style={"font-weight":"bolder","color":"#0075C2","text-align": "center", "position": "absolute", "top": "55%", "left": "50%", "transform": "translate(-50%, -50%)"}),
                                         ],
                                         style={"position": "relative", "height": "100%"}
                                     ),
@@ -41,8 +41,8 @@ block1 = html.Div(
                                         className="card card-container",
                                         children=[
                                             html.Div(className="plots", children=[dcc.Graph(id='new_entries_race_plot', className='race_plot')]),
-                                            html.H3(children="New Program Entries", className='display-5', style={"text-align": "center", "position": "absolute", "top": "45%", "left": "50%", "transform": "translate(-50%, -50%)"}),
-                                            html.H3(id="new_entries_count",className="metric", style={"text-align": "center", "position": "absolute", "top": "55%", "left": "50%", "transform": "translate(-50%, -50%)"}),
+                                            html.H3(children="New Program Entries", className='display-5', style={"background-color":'#0075C2',"color":"white","text-align": "center", "position": "absolute", "top": "45%", "left": "50%", "transform": "translate(-50%, -50%)"}),
+                                            html.H3(id="new_entries_count",className="metric", style={"font-weight":"bolder","color":"#0075C2","text-align": "center", "position": "absolute", "top": "55%", "left": "50%", "transform": "translate(-50%, -50%)"}),
                                         ],
                                         style={"position": "relative", "height": "100%"}
                                     ),
@@ -53,8 +53,8 @@ block1 = html.Div(
                                         className="card card-container",
                                         children=[
                                             html.Div(className="plots", children=[dcc.Graph(id='new_referrals_race_plot', className='race_plot')]),
-                                            html.H3(children="New Referrals to Housing Queue", className='display-5', style={"text-align": "center", "position": "absolute", "top": "45%", "left": "50%", "transform": "translate(-50%, -50%)"}),
-                                            html.H3(id="new_referrals_count", className="metric", style={"text-align": "center", "position": "absolute", "top": "55%", "left": "50%", "transform": "translate(-50%, -50%)"}),
+                                            html.H3(children="New Referrals to Housing Queue", className='display-5', style={"background-color":'#0075C2',"color":"white","text-align": "center", "position": "absolute", "top": "45%", "left": "50%", "transform": "translate(-50%, -50%)"}),
+                                            html.H3(id="new_referrals_count", className="metric", style={"font-weight":"bolder","color":"#0075C2","text-align": "center", "position": "absolute", "top": "55%", "left": "50%", "transform": "translate(-50%, -50%)"}),
                                         ],
                                         style={"position": "relative", "height": "100%"}
                                     ),
@@ -69,60 +69,8 @@ block1 = html.Div(
                     style={"height": "100%"} # set the height of the card body container to 100%
                 ),
             ],
-            style={"background-color": "#ADC48B", "border-color": "#36413E", "opacity": "0.9"} # set the card background color and outline color
+            style={"background-color": "white", "border-color": "white", "opacity": "0.9"} # set the card background color and outline color
         ),
     ],
     style={"height": "100%"} # set the height of the main container to 100%
 )
-
-
-# from dash import Dash, html, dcc, Input, Output
-# import dash_bootstrap_components as dbc
-
-# block1 = html.Div(
-#     className="block-1",
-#     children=[
-#         html.Div(
-#             className="title-section-1",
-#             children=[html.H2(children="What's new?")]
-#         ),
-#         html.Div(
-#             className="body-section-1",
-#             children=[
-#                 html.Div(
-#                     id='first_card',
-#                     className="card",
-#                     children=[
-#                         html.H3(id="fth_count", className="metric"),
-#                         html.H3(children="First Time Homeless"),
-#                         html.Div(className="plots", children=[dcc.Graph(id='fth_race_plot', className='race_plot')]),
-#                     ]
-#                 ),
-#                 html.Div(
-#                     className="card",
-#                     children=[
-#                         html.H3(id="housed_count", className="metric"),
-#                         html.H3(children="Persons Housed"),
-#                         html.Div(className="plots", children=[dcc.Graph(id='housed_race_plot', className='race_plot')]),
-#                     ]
-#                 ),
-#                 html.Div(
-#                     className="card",
-#                     children=[
-#                         html.H3(id="new_entries_count",className="metric"),
-#                         html.H3(children="New Program Entries"),
-#                         html.Div(className="plots", children=[dcc.Graph(id='new_entries_race_plot', className='race_plot')]),
-#                     ]
-#                 ),
-#                 html.Div(
-#                     className="card",
-#                     children=[
-#                         html.H3(id="new_referrals_count", className="metric"),
-#                         html.H3(children="New Referrals to Housing Queue"),
-#                         html.Div(className="plots", children=[dcc.Graph(id='new_referrals_race_plot', className='race_plot')]),
-#                     ]
-#                 )
-#             ]
-#         )
-#     ]
-# )
