acc93105e6729efb08f4d006b48edb8736bb5277
cdeleon13 <115028409+cdeleon13@users.noreply.github.com>
2024-04-15 15:00:05 -0700

    Update pit_racial_equity_block.py

--- pit_racial_equity_block.py
+++ pit_racial_equity_block.py
@@ -6,65 +6,25 @@ pit_re_block = html.Div(
         html.Div(
             className="re-block-1",
             children=[
-                dbc.Card(
-                    children=[
-                        dbc.CardHeader("Racial Equity Statements on Sheltered PIT Data", className='display-4 text-white'),
-                        dbc.CardBody(
-                            children=[
-                                dbc.Row(
-                                    children=[
-                                        dbc.Col(
-                                            dbc.Card(
-                                                children=[
-                                                    dbc.CardHeader(children="Population Percent Statement"),
-                                                    dbc.CardBody(html.P(id='sheltered_pit_2'))
-                                                ], className="mb-3"
-                                            ),
-                                        ),
-                                        dbc.Col(
-                                            dbc.Card(
-                                                children=[
-                                                    dbc.CardHeader("Likelihood Statement"),
-                                                    dbc.CardBody(html.P(id='sheltered_pit_1'))
-                                                ]
-                                            ),
-                                        )
-                                    ]
-                                )
-                            ],                                                    
-                        )
-                    ], color="#334E58", outline=False, style={"margin":"1vw 0"}
-                ),   
-                
-                dbc.Card(
-                    children=[
-                        dbc.CardHeader("Racial Equity Statements on Unsheltered PIT Data", className='display-4 text-dark'),
-                        dbc.CardBody(
-                            children=[
-                                dbc.Row(
-                                    children=[
-                                        dbc.Col(
-                                            dbc.Card(
-                                                children=[
-                                                    dbc.CardHeader("Population Percent Statement"),
-                                                    dbc.CardBody(html.P(id='unsheltered_pit_2'))
-                                                ],
-                                            ),
-                                        ),
-                                        dbc.Col(
-                                            dbc.Card(
-                                                children=[
-                                                    dbc.CardHeader("Likelihood Statement"),
-                                                    dbc.CardBody(html.P(id='unsheltered_pit_1'))
-                                                ]
-                                            ),
-                                        )
-                                    ]
-                                )
-                            ]                                                        
-                        )
-                    ], color="#BED9EC", outline=False, style={"margin":"1vw 0"}
+                html.H2(
+                        children=[html.P("PIT Population Percent Statement", className='display-6', style={"color":"white","background-color":"#0075C2"})]
                 ),
+                html.H1(
+                        children=[html.P(id="race_equity_pit_statement", className='display-4', style={"color":"#0075C2"})]
+                ), 
+                html.H2(
+                        children=[html.P("Racial Equity Statement on Sheltered PIT Data", className='display-6', style={"color":"white","background-color":"#F8C02A"})]
+                ),
+                html.H1(
+                        children=[html.P(id="sheltered_pit_1", className='display-4', style={"color":"#F8C02A"})]
+                ), 
+                html.H2(
+                        children=[html.P("Racial Equity Statement on Unsheltered PIT Data", className='display-6', style={"color":"white","background-color":"#8D3188"})]
+                ),
+                html.H1(
+                        children=[html.P(id="unsheltered_pit_1", className='display-4', style={"color":"#8D3188"})]
+                ),
+                html.P("*The logic for constructing these equity statements was adopted from the Community Action Plan.") 
             ],
             style={"height": "100%"}
         ),
