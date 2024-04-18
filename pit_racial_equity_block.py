a67cbe9e6a807b3007344e2edfe141c9c43afd85
cdeleon13 <115028409+cdeleon13@users.noreply.github.com>
2024-04-17 17:16:16 -0700

    Update pit_racial_equity_block.py

--- pit_racial_equity_block.py
+++ pit_racial_equity_block.py
@@ -1,3 +1,4 @@
+# pit_racial_equity_block.py
 import dash_bootstrap_components as dbc
 from dash import html
 
@@ -7,24 +8,12 @@ pit_re_block = html.Div(
             className="re-block-1",
             children=[
                 html.H2(
-                        children=[html.P("PIT Population Percent Statement", className='display-6', style={"color":"white","background-color":"#0075C2"})]
+                    children=[html.P("PIT Population Percent Statement", className='display-6', style={"color":"white","background-color":"#0075C2"})]
                 ),
                 html.H1(
-                        children=[html.P(id="race_equity_pit_statement", className='display-4', style={"color":"#0075C2"})]
-                ), 
-                html.H2(
-                        children=[html.P("Racial Equity Statement on Sheltered PIT Data", className='display-6', style={"color":"white","background-color":"#F8C02A"})]
+                    children=[html.P(id="race_equity_pit_statement", className='display-4', style={"color":"#0075C2"})]
                 ),
-                html.H1(
-                        children=[html.P(id="sheltered_pit_1", className='display-4', style={"color":"#F8C02A"})]
-                ), 
-                html.H2(
-                        children=[html.P("Racial Equity Statement on Unsheltered PIT Data", className='display-6', style={"color":"white","background-color":"#8D3188"})]
-                ),
-                html.H1(
-                        children=[html.P(id="unsheltered_pit_1", className='display-4', style={"color":"#8D3188"})]
-                ),
-                html.P("*The logic for constructing these equity statements was adopted from the Community Action Plan.") 
+                html.Div(id='equity_statements'),  # Container for conditionally displayed content
             ],
             style={"height": "100%"}
         ),
